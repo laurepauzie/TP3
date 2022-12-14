c071a688a14d8ecbebf2fce830344353ab05614b
laurepauzie <laurepauzie@gmail.com>
2022-12-14 10:18:20 +0100

    Ajout de la classe Commande début, pas encore de set_valide ni surcharger

--- commande.cpp
+++ commande.cpp
@@ -1,5 +1,17 @@
 #include "commande.h"
 
 
-Commande::Commande() : {
+Commande::Commande(Client c1, std::vector<Produit> produits_achetes, bool valide) : _c1(c1), _produits_achetes(produits_achetes), _valide(valide) {
+}
+
+Client Commande::get_client(){
+	return _c1;
+}
+
+std::vector<Produit> Commande::get_liste_produits(){
+	return _produits_achetes;
+}
+
+bool get_valide(){
+	return _valide;
 }
--- commande.h
+++ commande.h
@@ -4,8 +4,16 @@
 
 class Commande{
 public:
-	Commande();
+	Commande(Client c1, std::vector<Produit> produits_achetes, bool valide);
+	Client get_client();
+	std::vector<Produit> get_liste_produits();
+	bool get_valide();
+
 private: 
+	Client c1;
+	std::vector<Produit> _produits_achetes;
+	bool _valide;
+
 };
 
 
