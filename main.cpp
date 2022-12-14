bb06eab155f5e4d1ac63eb4f1ac59cdb2284a7b6
louka <louka.chapus@gmail.com>
2022-12-14 09:44:18 +0100

    Ajout de la classe client valide, test dans le main et ajout de client.cpp dans le makefile

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <vector>
 #include "produit.h"
+#include "client.h"
 
 int main()
 {
 	std::cout << "test" << std::endl;
 
-	Produit("PS4","Console de jeu",10,499.99);
-
+	Produit p("PS4","Console de jeu",10,499.99);
+	
+	std::vector<Produit> pc;
+	Client c1("lchapus","CHAPUS","Louka",pc);
+	
 	return 0;
 }
